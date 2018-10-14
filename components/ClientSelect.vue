5ffbd971c1e327f90eb30def8e423624d52cd89a
Fredrik Mäkilä <fredrik.makila@uniqode.se>
2018-10-14 23:58:33 +0100

    Fix styling of multiselect and create new ClientSelect component

---
+++ components/ClientSelect.vue
@@ -0,0 +1,52 @@
+<template>
+  <multiselect
+    v-model="selectedClient"
+    :options="clients"
+    :close-on-select="true"
+    :clear-on-select="false"
+    :allow-empty="false"
+    :preserve-search="true"
+    name="clients"
+    placeholder="Select client"
+    label="name"
+    track-by="id"
+    class="input mb-2"
+    @input="$emit('input', selectedClient)">
+    <div slot="noResult">
+      <span>No such client found.</span>
+
+      <button @click="handleInput">
+        Create new client
+      </button>
+
+    </div>
+  </multiselect>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect
+  },
+
+  props: ['value', 'clients'],
+
+  data () {
+    return {
+      selectedClient: null
+    }
+  },
+
+  mounted () {
+    this.selectedClient = this.value
+  },
+
+  methods: {
+    handleInput () {
+      console.log('hej')
+    }
+  }
+}
+</script>
--- pages/registration/index.vue
+++ pages/registration/index.vue
@@ -1,140 +1,138 @@
 <template>
-  <div class="w-full flex">
-    <div class="flex flex-col w-full mt-4 mx-16 mb-24">
-      <div class="flex flex-row">
-        <div class="flex flex-col flex-1 mx-2">
-          <label
-            for="name"
-            class="label">Name</label>
-          <input
-            v-model="form.name"
-            type="text"
-            name="name"
-            class="input mb-2"
-            disabled="disabled">
+  <form @submit.prevent="onSubmit">
+    <div class="w-full flex">
+
+      <div class="flex flex-col w-full mt-4 mx-16 mb-24">
+        <div class="flex flex-row">
+          <div class="flex flex-col flex-1 mx-2">
+            <label
+              for="name"
+              class="label">Name</label>
+            <input
+              v-model="form.name"
+              type="text"
+              name="name"
+              class="input mb-2"
+              disabled="disabled">
+          </div>
+
+          <div class="flex flex-col flex-1 mx-2">
+            <label
+              for="email"
+              class="label">Email</label>
+            <input
+              v-model="form.email"
+              class="input mb-2"
+              type="text"
+              name="email"
+              disabled="disabled">
+          </div>
         </div>
 
-        <div class="flex flex-col flex-1 mx-2">
+        <div class="flex flex-row">
+          <div class="flex flex-col flex-1 mx-2">
+            <label
+              for="title"
+              class="label">Professional Title</label>
+            <input
+              v-model="form.title"
+              type="text"
+              name="title"
+              class="input mb-2"
+              placeholder="Professional title">
+          </div>
+
+          <div class="flex flex-col flex-1 mx-2">
+            <label
+              for="phoneNumber"
+              class="label">Phone Number</label>
+            <input
+              v-model="form.phoneNumber"
+              type="tel"
+              name="phoneNumber"
+              class="input mb-2"
+              placeholder="Phone number">
+          </div>
+        </div>
+
+        <div class="mx-2">
           <label
-            for="email"
-            class="label">Email</label>
-          <input
-            v-model="form.email"
-            class="input mb-2"
-            type="text"
-            name="email"
-            disabled="disabled">
+            for="clients"
+            class="label">Current Client
+          </label>
+          <client-select
+            v-model="selectedClient"
+            :clients="clients"
+            name="clients"
+            @input="onClientSelect" />
+
+          <label
+            for="address"
+            class="label">Address</label>
+          <places-auto-complete
+            v-model="selectedAddress"
+            :disabled="!selectedClient"
+            name="address"
+            class="w-full mb-4"/>
+        </div>
+
+        <div class="mx-2">
+          <breaq-map
+            v-if="selectedAddress"
+            :location="selectedAddress.location"
+            style="height: 20em;"
+            class="border border-grey-light rounded-sm" />
         </div>
       </div>
 
-      <div class="flex flex-row">
-        <div class="flex flex-col flex-1 mx-2">
-          <label
-            for="title"
-            class="label">Professional Title</label>
-          <input
-            v-model="form.title"
-            type="text"
-            name="title"
-            class="input mb-2"
-            placeholder="Professional title">
-        </div>
-
-        <div class="flex flex-col flex-1 mx-2">
-          <label
-            for="phoneNumber"
-            class="label">Phone Number*</label>
-          <input
-            v-model="form.phoneNumber"
-            type="tel"
-            name="phoneNumber"
-            class="input mb-2"
-            placeholder="Phone number">
+      <div class="fixed w-full pin-b bg-grey-lightest border-t border-grey-light h-16 z-20">
+        <div class="flex justify-end items-center h-full">
+          <button class="border border-blue-dark bg-blue-light shadow-md p-2 mr-8 rounded-sm text-white shadow-md">
+            Complete profile
+          </button>
         </div>
       </div>
 
-      <div class="mx-2">
-        <label
-          for="clients"
-          class="label">Current Client
-        </label>
-        <multiselect
-          v-model="selectedClient"
-          :options="clients"
-          :close-on-select="true"
-          name="clients"
-          placeholder="Select one"
-          label="name"
-          track-by="name"
-          class="input mb-2"
-          @input="onClientSelect" />
-
-        <label
-          for="address"
-          class="label">Address</label>
-        <places-auto-complete
-          v-model="selectedAddress"
-          :disabled="!selectedClient"
-          name="address"
-          class="w-full mb-4"/>
-      </div>
-
-      <div class="mx-2">
-        <breaq-map
-          v-if="selectedAddress"
-          :location="selectedAddress.location"
-          style="height: 20em;"
-          class="border border-grey-light rounded-sm" />
-      </div>
+      <portal to="modals">
+        <modal
+          :open="openSelectAddressModal"
+          @close="openSelectAddressModal = false">
+          <div v-if="selectedClient">
+            <h2>Select an address for {{ selectedClient.name }} </h2>
+            <ul class="list-reset">
+              <li
+                v-for="address in clientAddresses"
+                :key="address.id"
+                class="p-2 cursor-pointer hover:bg-blue-light hover:text-white"
+                @click.prevent="addressSelected(address)">
+                {{ address.formattedAddress }}
+              </li>
+            </ul>
+          </div>
+        </modal>
+      </portal>
     </div>
-
-    <div class="fixed w-full pin-b bg-grey-lightest border-t border-grey-light h-16 z-20">
-      <div class="flex justify-end items-center h-full">
-        <button class="border border-blue-dark bg-blue-light shadow-md p-2 mr-8 rounded-sm text-white shadow-md">
-          Complete profile
-        </button>
-      </div>
-    </div>
-
-    <portal to="modals">
-      <modal
-        :open="openSelectAddressModal"
-        @close="openSelectAddressModal = false">
-        <div v-if="selectedClient">
-          <h2>Select an address for {{ selectedClient.name }} </h2>
-          <ul class="list-reset">
-            <li
-              v-for="address in clientAddresses"
-              :key="address.id"
-              class="p-2 cursor-pointer hover:bg-blue-light hover:text-white"
-              @click="addressSelected(address)">
-              {{ address.formattedAddress }}
-            </li>
-          </ul>
-        </div>
-      </modal>
-    </portal>
-  </div>
+  </form>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import { db } from '~/plugins/vuefire'
-import Multiselect from 'vue-multiselect'
 import { Portal } from 'portal-vue'
 import PlacesAutoComplete from '~/components/PlacesAutoComplete.vue'
 import Modal from '~/components/Modal.vue'
 import BreaqMap from '~/components/Map.vue'
+import ClientSelect from '~/components/ClientSelect.vue'
 
 export default {
   name: 'Registration',
 
   components: {
-    Multiselect,
     PlacesAutoComplete,
     Portal,
     Modal,
-    BreaqMap
+    BreaqMap,
+    ClientSelect
   },
 
   data () {
@@ -181,6 +179,24 @@ export default {
     addressSelected (address) {
       this.selectedAddress = address
       this.openSelectAddressModal = false
+    },
+
+    async onSubmit () {
+      await this.addClientLocation()
+      await this.updateProfile()
+    },
+    async addClientLocation () {
+      const snapshots = await db.collection('clients').doc(this.selectedClient.id).collection('addresses').get()
+      const addresses = []
+      snapshots.forEach(doc => addresses.push(doc.data()))
+      const notExists = !addresses.find(address => address.id === this.selectedAddress.id)
+
+      if (notExists) {
+        db.collection('clients').doc(this.selectedClient.id).collection('addresses').add(this.selectedAddress)
+      }
+    },
+    async updateProfile () {
+      db.collection('users').doc(this.profile.uid).update(this.form)
     }
   }
 }
