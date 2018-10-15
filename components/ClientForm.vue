668d1469797f7ee2afa7dc35b8c5df8fb82b4f86
Fredrik Mäkilä <fredrik.makila@uniqode.se>
2018-10-15 23:54:52 +0100

    Basic form to create a new client

--- components/ClientForm.vue
+++ components/ClientForm.vue
@@ -2,75 +2,60 @@
   <form
     class="w-full">
     <div class="flex flex-wrap mb-6">
-      <div class="w-full px-3 mb-6 md:mb-0">
+      <div class="w-full mb-6">
         <label
-          class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+          class="label"
           for="client-name">
           Client name
         </label>
         <input
           id="client-name"
-          v-model="client.name"
-          :class="{ 'border-red': errors.name }"
-          class="appearance-none block w-full bg-grey-lighter text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          v-model="form.name"
+          class="input w-full"
           type="text"
           placeholder="Client name">
-        <p
-          v-if="errors.name"
-          class="text-red text-xs italic">Please fill out this field.</p>
       </div>
-      <div class="w-full px-3 mb-6 md:mb-0">
+      <div class="w-full mb-2">
         <label
-          class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+          class="label"
           for="client-address">
           Client Address
         </label>
         <places-auto-complete
-          id="client-address"
-          @input="addAddress" />
-        <div/>
-        <div
-          v-if="addresses.length"
-          class="border-t border-grey-light">
-          <div
-            v-for="(address, index) in addresses"
-            :key="address.id"
-            class="text-sm w-full flex justify-between border-b border-grey-light pb-2 pt-2">
-            <span class="leading-loose">{{ address.formattedAddress }}</span>
+          v-model="form.address"
+          name="client-address"
+          class="input w-full" />
 
-            <button
-              class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
-              @click.prevent="removeAddress(index)">
-              <delete-icon />
-            </button>
-          </div>
-        </div>
       </div>
     </div>
+
+    <breaq-map
+      v-if="form.address"
+      :location="form.address.location"
+      style="height: 20em;"
+      class="border border-grey-light rounded-sm" />
   </form>
 
 </template>
 
 <script>
 import PlacesAutoComplete from '~/components/PlacesAutoComplete.vue'
-import DeleteIcon from '~/components/DeleteIcon.vue'
+import BreaqMap from '~/components/Map.vue'
 
 export default {
   components: {
     PlacesAutoComplete,
-    DeleteIcon
+    BreaqMap
   },
 
-  props: ['client', 'addresses', 'errors'],
+  props: ['client'],
 
-  methods: {
-    addAddress (newAddress) {
-      if (!this.addresses.find(address => address.id === newAddress.id)) {
-        this.addresses.push(newAddress)
+  data () {
+    return {
+      form: {
+        name: '',
+        address: null
       }
-    },
-    removeAddress (index) {
-      this.addresses.splice(index, 1)
     }
   }
 }
--- components/ClientSelect.vue
+++ components/ClientSelect.vue
@@ -1,33 +1,53 @@
 <template>
-  <multiselect
-    v-model="selectedClient"
-    :options="clients"
-    :close-on-select="true"
-    :clear-on-select="false"
-    :allow-empty="false"
-    :preserve-search="true"
-    name="clients"
-    placeholder="Select client"
-    label="name"
-    track-by="id"
-    class="input mb-2"
-    @input="$emit('input', selectedClient)">
-    <div slot="noResult">
-      <span>No such client found.</span>
+  <div>
+    <multiselect
+      v-model="selectedClient"
+      :options="clients"
+      :close-on-select="true"
+      :clear-on-select="false"
+      :allow-empty="false"
+      :preserve-search="true"
+      name="clients"
+      placeholder="Select client"
+      label="name"
+      track-by="id"
+      class="input mb-2"
+      @input="$emit('input', selectedClient)"
+      @search-change="handleSearch">
+      <div
+        slot="noResult"
+        class="p-2">
+        <span class="block">No such client found.</span>
 
-      <button @click="handleInput">
-        Create new client
-      </button>
+        <button
+          class="m-2 border border-grey shadow-md p-2 rounded-sm bg-blue text-white"
+          @click.prevent="openForm = true">
+          Create new client
+        </button>
 
-    </div>
-  </multiselect>
+      </div>
+    </multiselect>
+    <portal to="modals">
+      <modal :open="openForm">
+        <client-form :client="search" />
+      </modal>
+    </portal>
+
+  </div>
+
 </template>
 
 <script>
+import { Portal } from 'portal-vue'
+import Modal from '~/components/Modal.vue'
+import ClientForm from '~/components/ClientForm.vue'
 import Multiselect from 'vue-multiselect'
 
 export default {
   components: {
+    Portal,
+    Modal,
+    ClientForm,
     Multiselect
   },
 
@@ -35,7 +55,9 @@ export default {
 
   data () {
     return {
-      selectedClient: null
+      selectedClient: { },
+      openForm: false,
+      search: ''
     }
   },
 
@@ -44,8 +66,8 @@ export default {
   },
 
   methods: {
-    handleInput () {
-      console.log('hej')
+    handleSearch (search) {
+      this.search = search
     }
   }
 }
