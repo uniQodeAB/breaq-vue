6efe561bf9a4e4c3cc9037cafb4cc45e6d29e8d9
Fredrik Mäkilä <fredrik.makila@uniqode.se>
2018-10-28 15:52:09 +0000

    Change layout according to Invision wireframes

--- pages/registration/index.vue
+++ pages/registration/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <div class="w-full flex">
-
-      <div class="flex flex-col w-full mt-4 mx-16 mb-24">
-        <div class="flex flex-wrap">
-          <div class="flex flex-col flex-1 mx-2">
+  <form
+    class="flex flex-col flex-grow"
+    @submit.prevent="onSubmit">
+    <div class="flex-grow">
+      <div>
+        <div class="flex flex-wrap mb-4">
+          <div class="w-full md:w-1/2 mb-4 px-3">
             <label
               for="name"
               class="label">Name</label>
@@ -12,37 +13,25 @@
               v-model="form.name"
               type="text"
               name="name"
-              class="input mb-2"
+              class="input w-full"
               disabled="disabled">
           </div>
 
-          <div class="flex flex-col flex-1 mx-2">
+          <div class="w-full md:w-1/2 px-3">
             <label
               for="email"
               class="label">Email</label>
             <input
               v-model="form.email"
-              class="input mb-2"
+              class="input w-full"
               type="text"
               name="email"
               disabled="disabled">
           </div>
         </div>
 
-        <div class="flex flex-row">
-          <div class="flex flex-col flex-1 mx-2">
-            <label
-              for="title"
-              class="label">Professional Title</label>
-            <input
-              v-model="form.title"
-              type="text"
-              name="title"
-              class="input mb-2"
-              placeholder="Professional title">
-          </div>
-
-          <div class="flex flex-col flex-1 mx-2">
+        <div class="flex flex-wrap mb-4">
+          <div class="w-full md:w-1/2 mb-4 px-3">
             <label
               for="phoneNumber"
               class="label">Phone Number</label>
@@ -50,68 +39,98 @@
               v-model="form.phoneNumber"
               type="tel"
               name="phoneNumber"
-              class="input mb-2"
+              class="input w-full"
               placeholder="Phone number">
           </div>
-        </div>
 
-        <div class="mx-2">
-          <label
-            for="clients"
-            class="label">Current Client
-          </label>
-          <client-select
-            v-model="selectedClient"
-            :clients="clients"
-            name="clients"
-            @input="onClientSelect" />
-
-          <label
-            for="address"
-            class="label">Address</label>
-          <places-auto-complete
-            v-model="selectedAddress"
-            :disabled="!selectedClient"
-            name="address"
-            class="w-full mb-4"/>
-        </div>
-
-        <div class="mx-2">
-          <breaq-map
-            v-if="selectedAddress"
-            :location="selectedAddress.location"
-            style="height: 20em;"
-            class="border border-grey-light rounded-sm" />
-        </div>
-      </div>
-
-      <div class="fixed w-full pin-b bg-grey-lightest border-t border-grey-light h-16 z-20">
-        <div class="flex justify-end items-center h-full">
-          <button class="border border-blue-dark bg-blue-light shadow-md p-2 mr-8 rounded-sm text-white shadow-md">
-            Complete profile
-          </button>
-        </div>
-      </div>
-
-      <portal to="modals">
-        <modal
-          :open="openSelectAddressModal"
-          @close="openSelectAddressModal = false">
-          <div v-if="selectedClient">
-            <h2>Select an address for {{ selectedClient.name }} </h2>
-            <ul class="list-reset">
-              <li
-                v-for="address in clientAddresses"
-                :key="address.id"
-                class="p-2 cursor-pointer hover:bg-blue-light hover:text-white"
-                @click.prevent="addressSelected(address)">
-                {{ address.formattedAddress }}
-              </li>
-            </ul>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              for="title"
+              class="label">Q-group company</label>
+            <input
+              v-model="form.title"
+              type="text"
+              name="title"
+              class="input w-full"
+              disabled="disabled">
           </div>
-        </modal>
-      </portal>
+
+        </div>
+
+        <div class="flex flex-wrap mb-4">
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              for="title"
+              class="label">Professional Title</label>
+            <input
+              v-model="form.title"
+              type="text"
+              name="title"
+              class="input w-full"
+              placeholder="Professional title">
+          </div>
+        </div>
+
+        <div class="flex flex-wrap mb-4">
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              for="clients"
+              class="label">Current Client
+            </label>
+            <client-select
+              v-model="selectedClient"
+              :clients="clients"
+              name="clients"
+              @input="onClientSelect" />
+          </div>
+
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              for="address"
+              class="label">Address</label>
+            <places-auto-complete
+              v-model="selectedAddress"
+              :disabled="!selectedClient"
+              name="address"
+              class="w-full"/>
+          </div>
+        </div>
+
+        <div class="flex flex-wrap mb-32">
+          <div class="w-full px-3">
+            <breaq-map
+              v-if="selectedAddress"
+              :location="selectedAddress.location"
+              style="height: 20em;"
+              class="border border-grey-light rounded-lg px-3" />
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="w-full flex justify-center mb-16">
+      <button class="button button--blue px-16">
+        Save
+      </button>
+    </div>
+
+    <portal to="modals">
+      <modal
+        :open="openSelectAddressModal"
+        @close="openSelectAddressModal = false">
+        <div v-if="selectedClient">
+          <h2>Select an address for {{ selectedClient.name }} </h2>
+          <ul class="list-reset">
+            <li
+              v-for="address in clientAddresses"
+              :key="address.id"
+              class="p-2 cursor-pointer hover:bg-blue-light hover:text-white"
+              @click.prevent="addressSelected(address)">
+              {{ address.formattedAddress }}
+            </li>
+          </ul>
+        </div>
+      </modal>
+    </portal>
   </form>
 </template>
 
