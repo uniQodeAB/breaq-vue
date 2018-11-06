9720adbfb62202461ab45f645a0c22dc9f63df99
Steven Hu <husteven.91@gmail.com>
2018-11-06 21:09:14 +0100

    Add page for consultant list

---
+++ pages/home/consultants/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mx-8">
+    <h1>
+      Consultants
+    </h1>
+    <consultant-list/>
+  </div>
+</template>
+<script>
+import ConsultantList from '~/components/ConsultantList.vue'
+
+export default {
+  components: {
+    ConsultantList
+  }
+}
+</script>
