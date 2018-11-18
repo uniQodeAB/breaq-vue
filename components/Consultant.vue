3e2a366a042f51c9064f8cc623a44ce3e57c8c18
Steven Hu <husteven.91@gmail.com>
2018-11-18 15:44:32 +0100

    Create consultant component

---
+++ components/Consultant.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex">
+    <div class="flex pin-l pin-t">
+      <img
+        :src="consultant.photoURL"
+        class="h-12 rounded-full">
+    </div>
+    <div class="flex-col pin-r pin-t mb-auto">
+      <!-- Make this bigger and bold -->
+      <h1>
+        {{ consultant.name }}
+      </h1>
+      <p>Proffesion at {{ consultant.company }}</p>
+      <p>Email</p>
+      <p>PhoneNumber</p>
+      <p>Works at {{ consultant.client.name }}</p>
+      <p>{{ consultant.clientAddress.formattedAddress }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['consultant']
+
+}
+</script>
--- components/ConsultantList.vue
+++ components/ConsultantList.vue
@@ -7,26 +7,7 @@
         v-for="consultant in consultants"
         :key="consultant.id"
         class="px-4 py-2 block text-black hover:bg-grey-light">
-
-        <!-- Refactor consultant to a component -->
-        <div class="flex">
-          <div class="flex pin-l pin-t">
-            <img
-              :src="consultant.photoURL"
-              class="h-12 rounded-full">
-          </div>
-          <div class="flex-col pin-r pin-t mb-auto">
-            <!-- Make this bigger and bold -->
-            <h1>
-              {{ consultant.name }}
-            </h1>
-            <p>Proffesion at {{ consultant.company }}</p>
-            <p>Email</p>
-            <p>PhoneNumber</p>
-            <p>Works at {{ consultant.client.name }}</p>
-            <p>{{ consultant.clientAddress.formattedAddress }}</p>
-          </div>
-        </div>
+        <consultant :consultant="consultant"></consultant>
       </li>
     </ul>
   </div>
@@ -34,8 +15,13 @@
 
 <script>
 import { db } from '~/plugins/vuefire'
+import consultant from '~/components/Consultant.vue'
 
 export default {
+  components: {
+    consultant
+  },
+
   data () {
     return {
       consultants: [],
@@ -47,6 +33,7 @@ export default {
     try {
       await this.$bind('consultants', db.collection('consultants').orderBy('createdAt'))
       this.loaded = true
+      console.log(this.consultants)
     } catch (error) {
       console.log('error in loading: ', error)
     }
