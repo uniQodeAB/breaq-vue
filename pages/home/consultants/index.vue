646a22435edd556dc282aff644146e3475db014d
Steven Hu <husteven.91@gmail.com>
2019-01-20 14:45:45 +0100

    Move firebse folder to root and change paths

--- pages/home/consultants/index.vue
+++ pages/home/consultants/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="mx-8">
-    <h1>
+    <h1 class="heading">
       Consultants
+      <nuxt-link
+        class="bg-blue hover:bg-blue-dark text-white font-bold px-4 rounded no-underline m-8"
+        to="/home/clients">
+        View Clients
+      </nuxt-link>
+      <nuxt-link
+        class="bg-blue hover:bg-blue-dark text-white font-bold px-4 rounded no-underline"
+        to="/">
+        View in Map
+      </nuxt-link>
     </h1>
     <consultant-list/>
   </div>
@@ -10,6 +20,7 @@
 import ConsultantList from '~/components/ConsultantList.vue'
 
 export default {
+  layout: 'main',
   components: {
     ConsultantList
   }
