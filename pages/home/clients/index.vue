f431eae61fd9400acf8da1c4f6d7b6692df8aae8
Steven Hu <husteven.91@gmail.com>
2019-01-20 14:45:45 +0100

    Move firebse folder to root and change paths

---
+++ pages/home/clients/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mx-8">
+    <h1 class="heading">
+      Clients
+    </h1>
+    <nuxt-link
+      to="/home/consultants">
+      View Consultants
+    </nuxt-link>
+    <nuxt-link
+      to="/">
+      View in Map
+    </nuxt-link>
+    <client-list/>
+  </div>
+</template>
+
+<script>
+import ClientList from '~/components/ClientList.vue'
+
+export default {
+  middleware: ['auth', 'check-profile'],
+  components: {
+    ClientList
+  }
+}
+</script>
