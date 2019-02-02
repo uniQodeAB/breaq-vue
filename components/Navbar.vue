ad37a4a91f44d7a06d455ffa913459830176ed68
Fredrik Mäkilä <fredrik.makila@uniqode.se>
2019-02-02 17:01:45 +0000

    Add navbar without functionality

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="w-full bg-white h-28 fixed pin-t z-10">
+  <nav class="navbar">
+    <span class="navbar-toggle">
+      <i class="fas fa-bars"></i>
+    </span>
+
+    <a
+      href="#"
+      class="logo">logo</a>
+
+    <ul class="main-nav">
+      <li>
+        <a
+          href="#"
+          class="nav-link">Home</a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="nav-link">Products</a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="nav-link">About Us</a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="nav-link">Contact Us</a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="nav-link">Blog</a>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- <div class="w-full bg-white h-28 fixed pin-t z-10">
     <div class="flex justify-between items-center h-12 mx-8 py-2 mt-12">
       <nuxt-link
         to="/"
@@ -16,7 +54,7 @@
           class="h-12 rounded-full">
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
