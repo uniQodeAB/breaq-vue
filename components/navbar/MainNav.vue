fe23190af046db117c19db423daff5edecdb895a
Fredrik Mäkilä <fredrik.makila@uniqode.se>
2019-02-02 20:26:30 +0000

    Close navbar when navlink is clicked

---
+++ components/navbar/MainNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul
+    :class="active ? 'sm:block' : 'sm:hidden'"
+    class="main-nav hidden md:flex">
+    <li
+      v-for="path in paths"
+      :key="path.name">
+
+      <nav-link
+        :path="path.path"
+        @navigate="$emit('navigate')">
+        {{ path.name }}
+      </nav-link>
+
+    </li>
+  </ul>
+</template>
+
+<script>
+import NavLink from '~/components/navbar/NavLink.vue'
+
+export default {
+  components: {
+    NavLink
+  },
+
+  props: {
+    active: {
+      type: Boolean,
+      required: true
+    },
+    paths: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
