740ac2051293e5b6ff12caee66d8decb2f980aa4
Steven Hu <husteven.91@gmail.com>
2019-02-10 13:35:33 +0100

    Remove buttons because we have a navbar

--- pages/consultants/index.vue
+++ pages/consultants/index.vue
@@ -2,16 +2,6 @@
   <div class="mx-8">
     <h1 class="heading">
       Consultants
-      <nuxt-link
-        class="bg-blue hover:bg-blue-dark text-white font-bold px-4 rounded no-underline m-8"
-        to="/home/clients">
-        View Clients
-      </nuxt-link>
-      <nuxt-link
-        class="bg-blue hover:bg-blue-dark text-white font-bold px-4 rounded no-underline"
-        to="/">
-        View in Map
-      </nuxt-link>
     </h1>
     <consultant-list/>
   </div>
